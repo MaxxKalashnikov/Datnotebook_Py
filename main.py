7ad8e2d05a79016ab3894668e0257de081942b93
Maks <max.vanholl@mail.ru>
2023-10-08 12:04:10 +0300

    Added all files

--- main.py
+++ main.py
@@ -1,54 +1,59 @@
-import math
-num11 = 0
-num22 = 0
+import time
+import pickle
 
-def initnum(num):
-    while True:
-        try:
-            num = int(input("Give a number: "))
-        except ValueError:
-            print("This input is invalid.")
-        else:
-            return num
+notebookList = []
+t = 0
+
+try:
+	opFile = open("notebook.dat", "rb")
+	t = 1
+
+except Exception:
+	opFile = open("notebook.dat", "wb")
+	print("No default notebook was found, created one.")
+
+if (t == 1):
+	justread = pickle.load(opFile)
+	notebookList.append(justread)
+
+while True:
+
+	print("(1) Read the notebook\n(2) Add note\n(3) Edit a note\n(4) Delete a note\n(5) Save and quit\n")
+
+	selec = int(input("Please select one: "))
+	if selec == 1:
+		for i in notebookList:
+			print(i)
+	elif selec == 2:
+		newNote = str(input("Write a new note: "))
+		nowTime = str(time.strftime("%X %x"))
+		result = newNote + ":::" + nowTime
+		notebookList.append(str(result))
+	elif selec == 3:
+		print("The list has",len(notebookList) ,"notes.")
+		chanItem = int(input("Which of them will be changed?: "))
+		chanNote = notebookList[chanItem]
+		notebookList.pop(chanItem)
+		print(chanNote)
+		newwNote = str(input("Give the new note: "))
+		nowTime = str(time.strftime("%X %x"))
+		newwNote = newwNote + ":::" + nowTime
+		notebookList.insert(chanItem, str(newwNote))
+	elif selec == 4:
+		print("The list has", len(notebookList), "notes.")
+		delItem = int(input("Which of them will be deleted?: "))
+		dellItem = notebookList[delItem-1]
+		notebookList.pop(delItem-1)
+		print("Deleted note", dellItem)
+	elif selec == 5:
+		opFile = open("notebook.dat", "ab")
+		pickle.dump(notebookList, opFile)
+		opFile.close()
+		print("Notebook shutting down, thank you.")
+		break
 
 
-def main():
-    print("Calculator")
-    num1 = initnum(num11)
-    num2 = initnum(num22)
-    while True:
-        print("(1) +\n(2) -\n(3) *\n(4) /\n(5)sin(number1/number2)\n(6)cos(number1/number2)\n(7)Change numbers\n(8)Quit")
-        print("Current numbers: ", num1, num2)
-        selec = int(input("Please select something (1-6): "))
-        if selec == 1:
-            print("The result is: ", (num1 + num2))
-            continue
-        elif selec == 2:
-            print("The result is: ", (num1 - num2))
-            continue
-        elif selec == 3:
-            print("The result is: ", (num1 * num2))
-            continue
-        elif selec == 4:
-            print("The result is: ", (num1 / num2))
-            continue
-        elif selec == 5:
-            ssin = math.sin((num1 / num2))
-            print("The result is: ", ssin)
-        elif selec == 6:
-            ccos = math.cos((num1 / num2))
-            print("The result is: ", ccos)
-        elif selec == 7:
-            num1 = initnum(num1)
-            num2 = initnum(num2)
-            continue
-        elif selec == 8:
-            print("Thank you!")
-            break
-        elif selec != 1 and selec != 2 and selec != 3 and selec != 4:
-            print("Selection was not correct.")
-            continue
-
-
-if __name__ == "__main__":
-    main()
+#opFile = open("notebook.dat", "rb")
+#justread = pickle.load(opFile)
+#print(justread)
+#opFile.close()
